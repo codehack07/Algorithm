428b76229c21017eab037e353e7c86071da7091e
anuj jain <anuj.aj07@gmail.com>
2015-07-07 12:21:52 +0530

    "commit 6th july"

---
+++ Algorithm/src/com/algo/LargestDiff.java
@@ -0,0 +1,61 @@
+package com.algo;
+
+public class LargestDiff {
+
+	public static void main(String args[])
+	{
+		int arr[] = {2, 3, 10, 6, 4, 8, 1};
+		
+		System.out.println(largestdiff(arr)+ " "+largestdiff1(arr));
+	}
+	
+	public static int largestdiff(int []arr)
+	{
+		int max_diff=arr[1]-arr[0];
+		int min=arr[0];
+		
+		for(int i=1;i<arr.length;i++)
+		{
+			if(arr[i]-min>max_diff)
+			{
+				max_diff=arr[i]-min;
+			}
+			if(arr[i]<min)
+				min=arr[i];
+		}
+		return max_diff;
+	}
+	
+   public static int largestdiff1(int []arr)
+   {
+	   int n = arr.length;
+	   int max_diff=-1000;
+	   int val = arr[n-1];
+	   
+	   for(int i=n-2;i>=0;i--)
+	   {
+		   
+		   if(arr[i]<val)
+		   {
+			   max_diff=max(max_diff,val-arr[i]);
+		   }
+		   else
+		   {
+			   val=arr[i];
+			   
+		   }
+	   }
+	  return max_diff; 
+   }
+   
+   public static int max(int m,int n)
+   {
+	   if(m>n)
+		   return m;
+	   else
+		   return n;
+	   
+   }
+
+}
+
---
+++ Algorithm/src/com/algo/PartialSort.java
@@ -0,0 +1,38 @@
+package com.algo;
+
+public class PartialSort {
+
+	public static void main(String args[])
+	{
+		int arr[] = {10, 20, 40, 30, 50, 60};
+		
+		partialsort(arr);
+	}
+	
+	public static void partialsort(int arr[])
+	{
+		
+		for(int i=arr.length-1;i>0;i--)
+		{
+			if(arr[i]<arr[i-1])
+			{
+			 int j=i-1;
+			 while(j>0&& arr[j]>arr[i])
+			 {
+				 j--;
+			     
+			}	
+			 swap(arr,i,j+1);
+			}
+		}
+		for(int i=0;i<arr.length;i++)
+		System.out.println(arr[i]);
+	}
+	
+	public static void swap(int arr[],int m,int n)
+	{
+		int temp=arr[m];
+		arr[m]=arr[n];
+		arr[n]=temp;
+	}
+}
