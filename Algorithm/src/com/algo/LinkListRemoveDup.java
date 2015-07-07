f2daef1f481d89a6872f6ad81b668dc04fe4a84b
anuj jain <anuj.aj07@gmail.com>
2015-07-07 22:07:51 +0530

    "commit new changes"

---
+++ Algorithm/src/com/algo/LinkListRemoveDup.java
@@ -0,0 +1,45 @@
+package com.algo;
+
+public class LinkListRemoveDup {
+
+	public static void main(String args[])
+	{
+	int length=8;
+	int arr[] = {1,2,2,2,3,4,5,5};
+	LinkedListNode[] nodes = new LinkedListNode[length];
+	
+	for (int i = 0; i < length; i++) {
+		nodes[i] = new LinkedListNode(arr[i], null, null);
+		}
+	for (int i = 0; i < length; i++) {
+		if (i < length - 1) {
+		nodes[i].setNext(nodes[i + 1]);
+		}
+		if (i > 0) {
+		nodes[i].setPrevious(nodes[i - 1]);
+		}
+		}
+	LinkedListNode head = nodes[0];
+	System.out.println(head.printForward());
+	head = removedup(head);
+	System.out.println(head.printForward());
+
+	}
+	
+	public static LinkedListNode removedup(LinkedListNode head)
+	{
+		LinkedListNode curr =head;
+		LinkedListNode next=null;
+		
+		while(curr.next!=null)
+		{
+			next=curr.next;
+			while( next!=null &&curr.data==next.data)
+				next=next.next;
+			curr.next=next;
+			if(next!=null)
+			curr=next;
+		}
+		return head;
+	}
+}
